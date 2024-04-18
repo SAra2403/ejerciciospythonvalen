7f53e82d5a7c1de71ddcad50cf3333b1a50f517c
SENA <SENA@IBAAPRCIIFSP901.FormacionSena.RED>
2024-04-18 10:04:01 -0500

    mi primer commit

---
+++ ejercicios.py
@@ -0,0 +1,25 @@
+A=5
+B=3
+suma= A+B
+print("la suma es", suma)
+
+
+A=55
+B=38
+resta= A-B
+print("la resta es", resta)
+
+S=80
+V=45
+multiplicacion= S*V
+print("la multipliacion es", multiplicacion)
+
+V=100
+S= 75
+division cociente= V/s
+print (" el resultado es", cociente )
+
+V= 90
+S= 60
+DIVISION RESIDUO= v%s
+print (" el resultado es", RESIDUO)
